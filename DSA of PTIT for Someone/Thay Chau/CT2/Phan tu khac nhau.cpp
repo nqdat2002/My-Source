fcb6969c30a3e1262f10b852f32e77759565fc4a
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2022-03-11 23:01:37 +0700

    Update Phan tu khac nhau.cpp

--- DSA of PTIT for Someone/Thay Chau/CT2/Phan tu khac nhau.cpp
+++ DSA of PTIT for Someone/Thay Chau/CT2/Phan tu khac nhau.cpp
@@ -1,18 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 int Binary_Search(int a[], int b[], int n){
-	int t = n; 
+	int index = n; 
 	int l = 0, r = n - 1;
 	while(l <= r){
 		int m = (l + r) / 2 ;
-		if(a[m] == b[m]) l = m + 1;
+		if(a[m] == b[m]) 
+			l = m + 1;
 		else{
-			t = m;
+			index = m;
 			r = m - 1; 
 		} 
 	} 
-	return t; 
+	return index; 
 }
 int main(){
 	int t; cin >> t;
