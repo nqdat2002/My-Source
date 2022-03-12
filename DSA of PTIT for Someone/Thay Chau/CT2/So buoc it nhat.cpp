90dcd3a7d83e6593548808895bd520df8890151e
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2022-03-12 11:27:53 +0700

    Update So buoc it nhat.cpp

--- DSA of PTIT for Someone/Thay Chau/CT2/So buoc it nhat.cpp
+++ DSA of PTIT for Someone/Thay Chau/CT2/So buoc it nhat.cpp
@@ -1,23 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int c[1001]; 
 int main(){
-	int t; cin >> t;
-	while(t--){
-		int n; cin >> n;
-		int a[n];
-		for(auto &x: a) cin >> x;
-		int res = 1; 
-		c[0] = 1; 
-		for(int i = 1; i < n; i ++){
-			c[i] = 1;
-			for(int j = 0; j < i; j ++){
-				if(a[i] >= a[j] && c[i] < c[j] + 1) 
-					c[i] = c[j] + 1; 
-			} 
-			res = max(res, c[i]); 
-		} 
-		cout << n - res << endl; 
-	} 
-	return 0; 
+    int t; cin >> t;
+    while(t--){
+    	int n; cin >> n;
+    	int a[n];
+    	for(int i = 0; i < n; i ++) cin >> a[i];
+    	int res = 1, f[1001];
+    	f[0] = 1;
+    	for(int i = 0; i < n; i ++){
+    		f[i] = 1;
+    		for(int j = 0; j < i; j ++){
+    			if(a[i] >= a[j])
+    				f[i] = max(f[i], f[j] + 1);
+    		}
+    		res = max(res, f[i]);
+    	}
+    	cout << n - res << endl;
+    }
+	return 0;
 }
