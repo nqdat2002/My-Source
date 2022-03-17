b85e2bcc31d5013bdb4a383d62c1d2b22b91bed9
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2022-03-18 00:36:00 +0700

    Update Hinh vuong lon nhat.cpp

--- DSA of PTIT for Someone/Thay Chau/CT3/Hinh vuong lon nhat.cpp
+++ DSA of PTIT for Someone/Thay Chau/CT3/Hinh vuong lon nhat.cpp
@@ -1,29 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-int f[1001][1001];
-int a[1001][1001]; 
-int dem(int n,int m){
-	int res = 0; 
-	for(int i = 0; i < n; i++) f[i][0] = a[i][0]; 
-	for(int i = 0; i < m; i++) f[0][i] = a[0][i];
-	
-	for(int i = 1; i < n; i++){
-		for(int j = 1; j < m; j++){
-			if(a[i][j] == 1)
-				f[i][j] = min(min(a[i - 1][j] + 1, f[i][j - 1] + 1), f[i - 1][j - 1] + 1); 
-			else f[i][j] = 0; 
-			res = max(res, f[i][j]);
-		}
-	} 
-	return res; 
-} 
+typedef long long ll;
 int main(){
-	int t; cin >> t;
-	while(t--){
-		int n, m;
-		cin >> n >> m; 
-		for(int i = 0; i < n; i++)
-			for(int j = 0; j < m; j++) cin >> a[i][j];
-		cout << dem(n, m) << endl; 
-	} 
+    int t; cin >> t;
+    while(t--){
+        int n, m;
+        cin>> n >> m;
+        int a[n][m];
+        ll f[n][m];
+        for(int i = 0; i < n; i ++)
+            for(int j = 0; j < m; j ++) cin >> a[i][j];
+        for(int i = 0; i < n; i ++){
+        	(a[i][0] == 1) ? f[i][0] = 1 : f[i][0] = 0;
+        	for(int j = 1; j < m; j ++){
+        		if(a[i][j])
+        			f[i][j] = f[i][j - 1] + 1;
+        		else 
+        			f[i][j] = 0;
+        	}
+		}
+		ll res = 0;
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j ++){
+				if(f[i][j] > res){
+					if(res > i + 1) continue;
+					ll tp = f[i][j];
+					for(int k = i; k > i - res - 1; k --)
+						tp = min(tp, f[k][j]);
+					res = max(tp, res);
+				}
+			}
+		}
+        cout << res << endl;
+    }
+    return 0;
 }
