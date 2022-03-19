4403f0684008c1629093edd28bce87333c46c595
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2022-03-20 01:14:42 +0700

    Update Catalan Number.py

--- DSA of PTIT for Someone/Thay Chau/CT3/Catalan Number.py
+++ DSA of PTIT for Someone/Thay Chau/CT3/Catalan Number.py
@@ -1,4 +1,8 @@
 import math
-n = int(input())
-a = math.factorial(2*n)//(math.factorial(n+1)*math.factorial(n))
-print(a)
+t = int(input())
+i = 1
+while i <= t:
+  n = int(input())
+  a = math.factorial(2*n)//(math.factorial(n+1)*math.factorial(n))
+  print(a)
+  i = i + 1;
