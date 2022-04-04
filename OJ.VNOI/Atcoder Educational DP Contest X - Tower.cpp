5b8ff9ae076a97e1a066f8d7ff4a30b31dca1ede
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2022-04-04 21:21:47 +0700

    Update Atcoder Educational DP Contest X - Tower.cpp

--- OJ.VNOI/Atcoder Educational DP Contest X - Tower.cpp
+++ OJ.VNOI/Atcoder Educational DP Contest X - Tower.cpp
@@ -1,24 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long ll;
-int dp[100005];
-vector<vector<int> > adj[100005];
-int DFS(int u){
-	if(dp(u)) return dp[u];
+ll dp[1005];
+vector<vector<ll> > adj(1005);
+ll dfs(int u){
+	if(dp[u]) return dp[u];
 	for(auto v: adj[u]){
-		dp[v] = DFS(v);
+		dp[v] = dfs(v);
 		dp[u] = max(dp[u], dp[v] + 1);
 	}
 	return dp[u];
 }
 int main(){
-	int n, m;
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+	int n, m; cin >> n >> m;
 	for(int i = 1; i <= m; ++i){
 		int u, v; cin >> u >> v;
-		a[u].push_back(v);
+		adj[u].push_back(v);
 	}
-	for(int u = 1; u <= n; ++u) DFS(u);
-	int res = 0;
+	for(int u = 1; u <= n; ++u) dfs(u);
+	ll res = 0;
 	for(int i = 1; i <= n; ++i) res = max(res, dp[i]);
 	cout << res << endl;
 	return 0;
