6a5d8716ef2aa3ead4cf2671dadbe0fc51614a10
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2022-04-07 20:37:54 +0700

    Update Hinh vuong lon nhat.cpp

--- DSA of PTIT for Someone/Thay Chau/CT3/Hinh vuong lon nhat.cpp
+++ DSA of PTIT for Someone/Thay Chau/CT3/Hinh vuong lon nhat.cpp
@@ -6,29 +6,19 @@ int main(){
     while(t--){
         int n, m;
         cin>> n >> m;
-        int a[n][m];
-        ll f[n][m];
-        for(int i = 0; i < n; i ++)
-            for(int j = 0; j < m; j ++) cin >> a[i][j];
-        for(int i = 0; i < n; i ++){
-        	(a[i][0] == 1) ? f[i][0] = 1 : f[i][0] = 0;
-        	for(int j = 1; j < m; j ++){
-        		if(a[i][j])
-        			f[i][j] = f[i][j - 1] + 1;
-        		else 
-        			f[i][j] = 0;
-        	}
-		}
-		ll res = 0;
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < m; j ++){
-				if(f[i][j] > res){
-					if(res > i + 1) continue;
-					ll tp = f[i][j];
-					for(int k = i; k > i - res - 1; k --)
-						tp = min(tp, f[k][j]);
-					res = max(tp, res);
-				}
+        int a[n + 1][m + 1];
+        ll f[n + 1][m + 1];
+        memset(f, 0, sizeof(f));
+        for(int i = 1; i <= n; i ++)
+            for(int j = 1; j <= m; j ++) cin >> a[i][j];
+        ll res = 0;
+        for(int i = 1; i <= m; ++i) f[0][i] = a[0][i];
+        for(int i = 1; i <= n; ++i) f[i][0] = a[i][0];
+        for(int i = 1; i <= n; ++i){
+        	for(int j = 1; j <= m; ++j) {
+        		if(a[i][j]) f[i][j] = min(min(f[i - 1][j], f[i][j - 1]), f[i - 1][j - 1]) + 1;
+        		else f[i][j] = 0;
+        		res = max(res, f[i][j]);
 			}
 		}
         cout << res << endl;
