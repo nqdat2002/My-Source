e7a18794f5d23a8ae10e8496c09c42e639b39c1e
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2022-05-01 12:06:33 +0700

    Create SUMMODK.cpp

---
+++ Luyencode.net/SUMMODK.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int main() {
+	int t = 1;
+	while (t--) {
+		int n, k; cin >> n >> k;
+		int dp[1005][1005];
+		dp[0][0] = 0;
+		for(int i = 1; i <= n; i++) dp[0][i] = -1e9;
+		for(int i = 1; i <= n; i++) {
+			int x; cin >> x;
+			x %= k;
+			for(int j = 0; j < k; j++)
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][(j + k - x) % k] + 1);
+		}
+		cout << dp[n][0] << endl;
+	}
+	return 0;
+}
