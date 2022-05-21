9a41f16128e63f1fdf768b829017b850985b472c
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2022-05-21 22:21:14 +0700

    Update Tich Lon Nhat.cpp

--- DSA of PTIT for Someone/Tich Lon Nhat.cpp
+++ DSA of PTIT for Someone/Tich Lon Nhat.cpp
@@ -1,16 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main(){
-	int t; cin >> t;
-	while(t--){
-		int n, m;
-		cin >> n >> m;
-		int a[n + 5], b[m + 5];
-		for(int i = 0; i < n; i ++) cin >> a[i];
-		sort(a, a + n);
-		for(int i = 0; i < m; i ++) cin >> b[i];
-		sort(b, b + m);
-		cout << (long long) a[n - 1] * b[0] << endl;
-	}
+	int n; cin >> n;
+	int a[n];
+	for(auto &x: a) cin >> x; 
+	sort(a, a + n);
+	cout << max(a[0] * a[1], max(a[0] * a[1] * a[n - 1], 
+			max(a[n - 1] * a[n - 2] * a[n - 3], a[n - 1] * a[n - 2])));
 	return 0;
 }
