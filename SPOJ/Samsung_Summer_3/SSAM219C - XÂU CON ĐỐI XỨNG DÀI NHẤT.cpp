96e1053dc245c26e7b70b575d57a4baa22819119
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2022-05-27 12:29:05 +0700

    Create SSAM219C - XÂU CON ĐỐI XỨNG DÀI NHẤT.cpp

---
+++ SPOJ/Samsung_Summer_3/SSAM219C - XÂU CON ĐỐI XỨNG DÀI NHẤT.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> p;
+typedef vector<vector<int> > vt;
+typedef vector<pair<int, int> > vp;
+
+const ll mod = 1e9 + 7;
+const int oo = 1e6 + 7;
+
+#define f first
+#define s second
+#define pb push_back
+#define ep emplace_back
+#define sz(a) (int) a.size()
+#define ms(s, n) memset(s, n, sizeof(s))
+#define present(t, x) (t.find(x) != t.end())
+#define all(a) (a.begin(), a.end())
+#define For(i, l, r) for (int i = l; i <= r; i++)
+#define Fod(i, r, l) for (int i = r; i >= l; i--)
+#define fillchar(a, x) memset(a, x, sizeof (a))
+#define faster ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+void FileIO(){
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+}
+
+// Author: Nguyen Quoc Dat
+// ID: B20DCPT053
+// Organization: PTIT
+// Date: 03/03/22 22:36
+// Program: Xau con doi xung dain nhat
+// -------------------  Your code STARTS here !!!---------------------------
+
+int f[1005][1005] = {0};
+string s;
+int dp(string s){
+    int n = s.length(), i, j, k, res = 1;
+    for (i = 0; i < n; i++)
+        f[i][i] = 1;
+    for (k = 1; k <= n - 1; k++) {
+        for (i = 0; i <= n - k - 1; i++) {
+            j = i + k;
+            if (s[i] == s[j] && k != 1)
+                f[i][j] = f[i + 1][j - 1];
+            else if (s[i] == s[j] && k == 1)
+                f[i][j] = 1;
+            else 
+                f[i][j] = 0;
+            
+            if (f[i][j] == 1)
+                res = max(res, j - i + 1);
+        }
+    }
+    return res;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int t; cin >> t;
+    while(t--){
+        cin >> s;
+        int n = s.length();
+        int res = 1;
+        for(int i = 0; i < n; i ++){
+            int u = i, v = i;
+            while(u >= 0 && v < n){
+                if(s[u] == s[v]){
+                    res = max(res, v - u + 1);
+                    --u;
+                    ++v;
+                }
+                else break;
+            }
+        }
+        for(int i = 0; i < n - 1; i ++){
+            int u = i, v = i + 1;
+            while(u >= 0 && v < n){
+                if(s[u] == s[v]){
+                    res = max(res, v - u + 1);
+                    --u;
+                    ++v;
+                }
+                else break;
+            }
+        }
+        cout << res << endl;
+    }
+    return 0;
+}
